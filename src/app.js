0e7c1f79c272915f5b8f97f4cfe74419aaffa52d
s6H4 <228504422+s6H4@users.noreply.github.com>
2025-08-29 19:13:13 +0300

    all

---
+++ src/app.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import Header from './components/Header.js';
+import Main from './components/Main.js';
+import Footer from './components/Footer.js';
+
+function App() {
+  return (
+    <div className="app">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>2025 Лимоны. Все права защищены.</p>
+    </footer>
+  );
+};
+
+export default Footer;
