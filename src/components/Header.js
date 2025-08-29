0a6fe9ffa49e3f6be0ee2b58d25919ca524bcc02
s6H4 <228504422+s6H4@users.noreply.github.com>
2025-08-29 19:39:12 +0300

    a

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,11 +3,11 @@ import React from 'react';
 const Header = () => {
   return (
     <header>
-      <h1>Lemonz</h1>
+      <h1>Lemons</h1>
       <nav>
-        <a href="#home" style={{ color: 'white', marginRight: '1rem' }}>Главная</a>
-        <a href="#about" style={{ color: 'white', marginRight: '1rem' }}>О нас</a>
-        <a href="#contact" style={{ color: 'white' }}>Контакты</a>
+        <a href="#home">Главная</a>
+        <a href="#about">О нас</a>
+        <a href="#contact">Контакты</a>
       </nav>
     </header>
   );
