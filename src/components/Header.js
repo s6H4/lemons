9b39bafb0ae3733d21167431781a86d9f7275882
whyffspacex <135847251+whyffspacex@users.noreply.github.com>
2025-09-03 22:15:02 +0500

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,14 +4,7 @@ import './../css/header.css';
 
 const Header = () => {
   return (
-    <header>
-      <h1>Lemons</h1>
-      <nav>
-        <Link to="/">Главная</Link>
-        <Link to="/about">О нас</Link>
-        <Link to="/info">Информация</Link>
-      </nav>
-    </header>
+динаху отсюда!!!
   );
 };
 
